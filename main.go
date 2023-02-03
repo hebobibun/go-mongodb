812c42f1bde42d90de07737391ebff1923615d7e
libobz <hebobibun@gmail.com>
2023-02-03 08:12:18 +0700

    feat : insert a new user

--- main.go
+++ main.go
@@ -2,11 +2,60 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"go-mongodb/config"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
-	clt := config.MgConnect()
-	defer clt.Disconnect(context.TODO())
+
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/create-user", createUser)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Name      string             `bson:"name" json:"name"`
+	Age       string             `bson:"age" json:"age"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+}
+
+func createUser(c echo.Context) error {
+	clt := config.MgConnect()
+	defer clt.Disconnect(context.TODO())
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var table = config.MgCollection("users", clt)
+
+	user1 := User{
+		Name:      "Pevita P",
+		Age:       "22",
+		CreatedAt: time.Now(),
+	}
+
+	_, err := table.InsertOne(ctx, &user1)
+	if err != nil {
+		fmt.Println("Failed to insert user")
+	}
+
+	fmt.Println(user1)
+
+	return c.String(http.StatusOK, "Inserted a new user successsfully")
+}
